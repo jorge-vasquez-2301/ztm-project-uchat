5af7e3a6a1196d313643cb55ec6149fb059812a3
Jorge Vasquez <jv2301@gmail.com>
2023-07-24 10:21:51 -0400

    Add DB functions for sessions

--- backend/query/src/lib.rs
+++ backend/query/src/lib.rs
@@ -14,4 +14,5 @@ pub use error::QueryError;
 pub mod util;
 pub use util::{AsyncConnection, AsyncConnectionPool, OwnedAsyncConnection};
 
+pub mod session;
 pub mod user;
---
+++ backend/query/src/session.rs
@@ -0,0 +1,73 @@
+use crate::DieselError;
+use chrono::{DateTime, Duration, Utc};
+use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
+use uchat_domain::ids::{SessionId, UserId};
+
+use crate::schema;
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize, DieselNewType)]
+pub struct FingerPrint(serde_json::Value);
+
+impl From<serde_json::Value> for FingerPrint {
+    fn from(value: serde_json::Value) -> Self {
+        Self(value)
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Queryable, Insertable)]
+#[diesel(table_name = schema::web)]
+pub struct Session {
+    pub id: SessionId,
+    pub user_id: UserId,
+    pub expires_at: DateTime<Utc>,
+    pub created_at: DateTime<Utc>,
+    pub fingerprint: FingerPrint,
+}
+
+pub fn new(
+    conn: &mut PgConnection,
+    user_id: UserId,
+    duration: Duration,
+    fingerprint: FingerPrint,
+) -> Result<Session, DieselError> {
+    let uid = user_id;
+    let now = Utc::now();
+    let new_session = Session {
+        id: SessionId::new(),
+        user_id: uid,
+        expires_at: now + duration,
+        created_at: now,
+        fingerprint,
+    };
+    {
+        use crate::schema::web;
+
+        diesel::insert_into(web::table)
+            .values(&new_session)
+            .on_conflict((web::user_id, web::fingerprint))
+            .do_update()
+            .set(web::expires_at.eq(new_session.expires_at))
+            .get_result(conn)
+    }
+}
+
+pub fn get(conn: &mut PgConnection, session_id: SessionId) -> Result<Option<Session>, DieselError> {
+    use crate::schema::web;
+    web::table
+        .filter(web::id.eq(session_id))
+        .get_result(conn)
+        .optional()
+}
+
+pub fn find(
+    conn: &mut PgConnection,
+    user_id: UserId,
+    fingerprint: FingerPrint,
+) -> Result<Session, DieselError> {
+    use crate::schema::web;
+    web::table
+        .filter(web::user_id.eq(user_id))
+        .filter(web::fingerprint.eq(fingerprint))
+        .get_result(conn)
+}
