458e2dab2d6fe1b3d1da70c51b2fe65a5baef2af
Jorge Vasquez <jv2301@gmail.com>
2023-08-15 10:51:53 -0400

    Show messages on liked and bookmarked pages

--- frontend/src/page/home/bookmarked.rs
+++ frontend/src/page/home/bookmarked.rs
@@ -18,6 +18,7 @@ pub fn HomeBookmarked(cx: Scope) -> Element {
             toaster
                 .write()
                 .info("Retrieving posts", Duration::seconds(3));
+            post_manager.write().clear();
             let response = fetch_json!(<BookmarkedPostsOk>, api_client, BookmarkedPosts);
             match response {
                 Ok(res) => post_manager.write().populate(res.posts.into_iter()),
@@ -29,7 +30,33 @@ pub fn HomeBookmarked(cx: Scope) -> Element {
         })
     };
 
-    let Posts = post_manager.read().all_to_public();
+    let Posts = {
+        let posts = post_manager.read().all_to_public();
+        if posts.is_empty() {
+            let TrendingLink = rsx! {
+                a {
+                    class: "link",
+                    onclick: move |_| {
+                        router.navigate_to(page::POSTS_TRENDING);
+                    },
+                    "trending"
+                }
+            };
+            rsx! {
+                div {
+                    class: "
+                        flex flex-col text-center justify-center
+                        h-[calc(100vh_-_var(--navbar-height)_-_var(--appbar-height))]
+                    ",
+                    span {
+                        "You don't have any bookmarked posts yet. Check out what's ",  TrendingLink ", and follow some users to get started."
+                    }
+                }
+            }
+        } else {
+            rsx! { posts.into_iter() }
+        }
+    };
 
     cx.render(rsx! {
         Appbar  {
@@ -55,6 +82,6 @@ pub fn HomeBookmarked(cx: Scope) -> Element {
                 title: "Go to the home page"
             }
         },
-        Posts.into_iter()
+        Posts
     })
 }
